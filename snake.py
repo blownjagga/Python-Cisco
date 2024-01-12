1b83462676a0db3ba450457e404f3be3166f4155
Seyi Oyedeji <Oyedejiseyi98@gmail.com>
2024-01-12 01:37:55 +0100

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,19 @@
+print ("Hisssssss...")
+
+print("Hello, world!")
+
+print("\\")
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+
+
+
+1 // 2 * 3
